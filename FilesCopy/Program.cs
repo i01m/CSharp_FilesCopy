8273df0202c916406b93ea3cec8b69754b585df2
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-16 09:43:51 -0500

    first commit

---
+++ FilesCopy/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace FilesCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //by default omit System.IO in example System.IO.Directory.Exists,
+            // can just type Directory.Exists since using System.IO
+
+            string fileName = "";
+            string sourcePath = @"c:\Project\DEVELOPMENT_VM\cSharpProjects\VisualStudio\FilesCopy\TestFolder\folder1";
+            string targetPath = @"c:\Project\DEVELOPMENT_VM\cSharpProjects\VisualStudio\FilesCopy\TestFolder\folder2";
+
+            string sourceFile = Path.Combine(sourcePath, fileName);
+            string destinationFile = Path.Combine(targetPath, fileName);
+
+            if (Directory.Exists(sourcePath))
+            {
+                if (Directory.Exists(targetPath))
+                {
+
+                    string[] files = Directory.GetFiles(sourcePath);
+
+                    foreach (string s in files)
+                    {
+                        fileName = Path.GetFileName(s);
+                        destinationFile = Path.Combine(targetPath, fileName);
+                        File.Copy(s, destinationFile, true);
+                    }
+                    Console.WriteLine("Files have been copied");
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to exit");
+                }
+
+                else
+                {
+                    Directory.CreateDirectory(targetPath);
+                    Console.WriteLine("target directory was created");
+                    string[] files = Directory.GetFiles(sourcePath);
+
+                    foreach (string s in files)
+                    {
+                        fileName = Path.GetFileName(s);
+                        destinationFile = Path.Combine(targetPath, fileName);
+                        File.Copy(s, destinationFile, true);
+                    }
+                    Console.WriteLine("Files have been copied");
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to exit");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Source Path does not exist!");
+
+            }
+
+                Console.ReadKey();
+                      
+        }
+    }
+}
